6ecf2d1742e9b1afbba24eacce56cd85c406b403
Alex Cline <rev.a.r.cline@gmail.com>
2021-04-27 14:39:27 -0500

    getting stuff installed

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  env: {
+    es6: true,
+    node: true,
+    browser: true,
+  },
+  parserOptions: {
+    ecmaVersion: 8,
+    sourceType: 'module',
+    allowImportExportEverywhere: true,
+    ecmaFeatures: {
+      jsx: true,
+      modules: true
+    },
+  },
+  ignorePatterns: ['/node_modules/**', '/build/**'],
+  rules: {
+    'no-unused-vars': ['warn', { args: 'none', argsIgnorePattern: 'req|res|next|val' }],
+    'prettier/prettier': ['warn'],
+    'react/prop-types': 'off',
+    'no-console': 'warn',
+    'no-debugger': 'warn',
+    'quotes': [2, 'single']
+  },  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
