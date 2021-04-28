6bf24baaf8dae39d266546c5f6f54dbb43dda1fe
Alex Cline <rev.a.r.cline@gmail.com>
2021-04-28 13:32:06 -0500

    trying again

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,3 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { combineReducers } from 'redux';
 
-const reducer = combineReducers({
-  // root reducer !
-});
-
-const store = configureStore({ reducer });
-
-export default store;
+export default configureStore({ rootReducer });
