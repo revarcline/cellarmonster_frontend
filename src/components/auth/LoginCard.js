0fa5f1c8b791f32ccb4a1a9b6232139759189e2c
Alex Cline <rev.a.r.cline@gmail.com>
2021-04-29 17:04:55 -0500

    non-working framework for login menu

--- src/components/auth/LoginCard.js
+++ src/components/auth/LoginCard.js
@@ -1,3 +1,55 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { loginUser } from '../../actions/auth';
+import { Card, Form, Button } from 'react-bootstrap';
+
+class LoginCard extends React.Component {
+  state = {
+    email: this.props.email,
+    password: '',
+    error: false,
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { email, password } = this.state;
+    this.props
+      .dispatchLoginUser({ email, password })
+      .then(() => this.props.history.push('/'))
+      .catch(() => this.setState({ error: true }));
+  };
+
+  render() {
+    return (
+      <Card body>
+        <Card.Title></Card.Title>
+        <Card.Subtitle></Card.Subtitle>
+        <Form>
+          <Form.Group>
+            <Form.Label>Passcode</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Passcode"
+              name="password"
+              onChange={this.handleChange}
+            />
+          </Form.Group>
+          <Button type="submit">Log In</Button>
+        </Form>
+      </Card>
+    );
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    dispatchLoginUser: (credentials) => dispatch(loginUser(credentials)),
+  };
+};
+export default connect(null, mapDispatchToProps)(LoginCard);
--- src/components/auth/LoginMenu.js
+++ src/components/auth/LoginMenu.js
@@ -1,5 +1,36 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { loginUser } from '../../actions/auth';
+import { usersList } from '../../actions/users';
+import LoginCard from './LoginCard';
+import { Spinner } from 'react-bootstrap';
 
-class LoginMenu extends React.Component {}
+class LoginMenu extends React.Component {
+  componentDidMount() {
+    this.props.usersList();
+  }
+
+  generateCards = () => {
+    // map props to LoginCard(s)
+  };
+
+  handleLoading = () => {
+    if (this.props.loading) {
+      return (
+        <Spinner animation="border" role="status">
+          <span className="sr-only">Loading...</span>
+        </Spinner>
+      );
+    } else {
+      return;
+    }
+  };
+}
+
+const mapDispatchToProps = (state) => {
+  return {
+    userList: state.users,
+    loading: state.loading,
+  };
+};
+
+export default connect(mapDispatchToProps, { usersList })(LoginMenu);
