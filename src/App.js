5e01b0244ef903da5b84206e76f9516bf337953d
Alex Cline <rev.a.r.cline@gmail.com>
2021-05-15 14:31:39 -0500

    using register instead

--- src/App.js
+++ src/App.js
@@ -23,7 +23,10 @@ function App({ loggedIn }) {
             <Col>
               <Switch>
                 <Route exact path="/" component={LoginMenu} />
-                <Route path="/users/new" component={withAuth(UserForm, { mode: 'new' })} />
+                <Route
+                  path="/users/new"
+                  component={withAuth(UserForm, { mode: 'new', editUser: {} })}
+                />
                 <Route path="/users/:id" component={withAuth(UserShow)} />
                 {/* <Route path="/orders" component={withAuth(OrderList)} /> */}
                 <Route path="/bottles" component={withAuth(BottleList)} />
--- src/components/auth/UserForm.js
+++ src/components/auth/UserForm.js
@@ -1,122 +1,93 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useEffect } from 'react';
 import { signupUser } from '../../actions/auth';
+import { useForm } from 'react-hook-form';
 import { Form, Button, Container, Row, Col } from 'react-bootstrap';
 
-class UserForm extends React.Component {
-  state =
-    this.props.mode === 'new'
+const UserForm = (props) => {
+  useEffect(() => console.log(props));
+
+  const initialValues =
+    props.mode === 'edit'
       ? {
+          email: props.editUser.email,
+          password: '',
+          name: props.editUser.name,
+          role: props.editUser.role,
+        }
+      : {
           email: '',
           password: '',
           name: '',
           role: 'server',
-        }
-      : {
-          email: this.props.editUser.email,
-          password: '',
-          name: this.props.editUser.name,
-          role: this.props.editUser.role,
         };
 
-  handleChange = (event) => {
-    this.setState({
-      [event.target.name]: event.target.value,
-    });
-  };
+  const { register, handleSubmit, errors } = useForm({ initialValues });
 
-  handleSubmit = (event) => {
-    event.preventDefault();
-    if (this.props.mode === 'new') {
-      const { email, password, name, role } = this.state;
-      this.props
-        .dispatchSignupUser({ email, password, name, role })
-        .then(() => this.props.history.push('/'))
-        .catch((errors) => this.setState({ errors }));
-    } else if (this.props.mode === 'edit') {
-      const { email, password, name, role } = this.state;
-      this.props
-        .dispatchUpdateUser({ email, password, name, role })
-        .catch((errors) => this.setState({ errors }));
+  const onSubmit = (data) => {
+    if (props.mode === 'new') {
+      console.log('new action}');
+    } else if (props.mode === 'edit') {
+      console.log('update action');
     }
+    console.log(JSON.stringify(data));
   };
 
-  render() {
-    return (
-      <Container className="pt-3" fluid>
-        <Row className="justify-content-md-center">
-          <Col xs md="8" sm="10" lg="6">
-            <Form onSubmit={this.handleSubmit}>
-              {this.props.mode === 'new' ? <h1>Create a New User</h1> : null}
-              <Form.Group>
-                <Form.Label htmlFor="email">Email:</Form.Label>
-                <Form.Control
-                  type="email"
-                  placeholder="email@domain.com"
-                  name="email"
-                  id="email"
-                  onChange={this.handleChange}
-                  value={this.state.email}
-                />
-              </Form.Group>
-              <Form.Group>
-                <Form.Label htmlFor="name">Name:</Form.Label>
-                <Form.Control
-                  type="text"
-                  name="name"
-                  placeholder="Enter Name"
-                  id="name"
-                  onChange={this.handleChange}
-                  value={this.state.name}
-                />
-              </Form.Group>
-              <Form.Group>
-                <Form.Label htmlFor="role">User Role:</Form.Label>
-                <Form.Control
-                  as="select"
-                  id="role"
-                  name="role"
-                  value={this.state.role}
-                  onChange={this.handleChange}
-                >
-                  <option value="server">Server</option>
-                  <option value="runner">Runner</option>
-                  <option value="admin">Admin</option>
-                </Form.Control>
-              </Form.Group>
-              <Form.Group>
-                <Form.Label htmlFor="password">Passcode:</Form.Label>
-                <Form.Control
-                  type="password"
-                  name="password"
-                  placeholder="Enter Passcode"
-                  id="password"
-                  onChange={this.handleChange}
-                  value={this.state.password}
-                />
-              </Form.Group>
-              <br />
+  return (
+    <Container className="pt-3" fluid>
+      <Row className="justify-content-md-center">
+        <Col xs md="8" sm="10" lg="6">
+          {props.mode === 'new' ? <h1>Create a New User</h1> : null}
+          <Form onSubmit={handleSubmit(onSubmit)}>
+            <Form.Group>
+              <Form.Label htmlFor="email">Email:</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="email@domain.com"
+                id="email"
+                {...register('email', { required: true })}
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label htmlFor="name">Name:</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter Name"
+                id="name"
+                {...register('name', { required: true })}
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label htmlFor="role">User Role:</Form.Label>
+              <Form.Control
+                as="select"
+                id="role"
+                name="role"
+                {...register('role', { required: true })}
+              >
+                <option value="server">Server</option>
+                <option value="runner">Runner</option>
+                <option value="admin">Admin</option>
+              </Form.Control>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label htmlFor="password">Passcode:</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Enter Passcode"
+                id="password"
+                {...register('password', { required: true })}
+              />
+            </Form.Group>
+            <br />
 
-              <Button type="submit" variant="primary" onSubmit={this.handleSubmit}>
-                Submit
-              </Button>
-            </Form>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
-}
-
-const mapStateToProps = (state) => {
-  // get user for edit
-  return {};
+            <Button type="submit" variant="primary">
+              Submit
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    dispatchSignupUser: (credentials) => dispatch(signupUser(credentials)),
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(UserForm);
+export default UserForm;
