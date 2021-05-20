3c443cfa9b8aa1ce4ed3fdab81b0ffd95d7444b2
Alex Cline <rev.a.r.cline@gmail.com>
2021-05-20 12:15:40 -0500

    woo

--- src/components/auth/withAuth.js
+++ src/components/auth/withAuth.js
@@ -1,40 +1,37 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { checkAuth } from '../../actions/auth';
 import { Spinner, Alert } from 'react-bootstrap';
 import LoginMenu from './LoginMenu';
 
 const withAuth = (WrappedComponent, role, extraProps) => {
-  class Wrapper extends React.Component {
-    componentDidMount() {
-      this.props.dispatchCheckAuth();
-    }
+  const Wrapper = (props) => {
+    const dispatch = useDispatch();
+    useEffect(() => {
+      handleCheckAuth();
+    });
 
-    render() {
-      if (!this.props.authChecked) {
-        return <Spinner animation="border" role="status" />;
-      } else if (!this.props.loggedIn) {
-        return (
-          <>
-            <Alert variant="primary">You must log in to use this feature.</Alert>
-            <LoginMenu />
-          </>
-        );
-      } else {
-        return <WrappedComponent {...this.props} {...extraProps} />;
-      }
-    }
-  }
+    const {
+      auth: { authChecked, loggedIn, currentUser },
+    } = useSelector((state) => state);
 
-  const mapStateToProps = ({ auth: { authChecked, loggedIn, currentUser } }) => {
-    return { authChecked, loggedIn, currentUser };
+    const handleCheckAuth = () => dispatch(checkAuth());
+
+    if (!authChecked) {
+      return <Spinner animation="border" role="status" />;
+    } else if (!loggedIn) {
+      return (
+        <>
+          <Alert variant="primary">You must log in to use this feature.</Alert>
+          <LoginMenu />
+        </>
+      );
+    } else {
+      return <WrappedComponent {...props} {...extraProps} />;
+    }
   };
 
-  const mapDispatchToProps = (dispatch) => {
-    return { dispatchCheckAuth: () => dispatch(checkAuth()) };
-  };
-
-  return connect(mapStateToProps, mapDispatchToProps)(Wrapper);
+  return Wrapper;
 };
 
 export default withAuth;
