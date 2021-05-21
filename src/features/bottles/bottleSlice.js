2f7ea2c545ea2375701fd1765de59bdd8ea57ea6
Alex Cline <rev.a.r.cline@gmail.com>
2021-05-21 13:12:31 -0500

    let's split api calls to one big file

--- src/features/bottles/bottleSlice.js
+++ src/features/bottles/bottleSlice.js
@@ -1,27 +1,48 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import apiRoot from '../../apiConfig';
+const axios = require('axios');
 
-const initialState = [];
+const initialState = { bottles: [], loading: 'idle', error: null };
 
-export const fetchBottles = createAsyncThunk('bottles/fetchBottles', async (resource, query) => {
+export const getBottles = createAsyncThunk('bottles/getBottles', async (resource, query) => {
   let params;
   if (query === '') {
     params = 'bottles';
   } else {
     params = `${resource}/${query}`;
   }
-  const res = await client.get(`${apiRoot}/${params}`);
-  return res.bottles;
+  axios
+    .get(`${apiRoot}/${params}`)
+    .then((response) => response.data)
+    .catch((error) => error);
 });
 
 const bottleSlice = createSlice({
   name: 'bottles',
   initialState,
   reducers: {
-    loadingBottles(state, action) {
-      return {
-        ...state,
-        bottleLoading: 'loading',
+    // non async logic go here
+  },
+  extraReducers: {
+    [getBottles.pending]: (state, action) => {
+      state.bottles = {
+        status: 'loading',
+        data: {},
+        error: {},
+      };
+    },
+    [getBottles.fulfilled]: (state, action) => {
+      state.bottles = {
+        status: 'finished',
+        data: action.payload,
+        error: {},
+      };
+    },
+    [getBottles.fulfilled]: (state, action) => {
+      state.bottles = {
+        status: 'finished',
+        data: {},
+        error: action.payload,
       };
     },
   },
