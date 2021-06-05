b1011f2c5aedc3e4144358dd4f39670f39f805c3
Alex Cline <rev.a.r.cline@gmail.com>
2021-06-05 16:30:26 -0500

    ok starting the hell slice

---
+++ src/features/auth/authSlice.js
@@ -0,0 +1,20 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import userAPI from '../userAPI';
+
+const initialState = {};
+
+const setToken = (token) => {
+  localStorage.setItem('token', token);
+  localStorage.setItem('lastLoginTime', new Date(Date.now()).getTime());
+};
+
+const getToken = () => {
+  const now = new Date(Date.now()).getTime();
+  const thirtyMinutes = 1000 * 60 * 30;
+  const timeSinceLastLogin = now - localStorage.getItem('lastLoginTime');
+  if (timeSinceLastLogin < thirtyMinutes) {
+    return localStorage.getItem('token');
+  }
+};
+
+export const signupUser = createAsyncThunk('auth/signupUser');
