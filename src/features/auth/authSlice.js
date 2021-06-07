9bffd56639ef324f51a0e515318ea17a3a1095a8
Alex Cline <rev.a.r.cline@gmail.com>
2021-06-07 14:52:28 -0500

    slowly but surely building that auth slice

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -1,7 +1,16 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import userAPI from '../userAPI';
 
-const initialState = {};
+const initialState = {
+  signup: { status: idle },
+  update: { status: idle },
+  authentication: {
+    authChecked: false,
+    loggedInloggedIn: false,
+    currentUser: {},
+    status: 'idle',
+  },
+};
 
 const setToken = (token) => {
   localStorage.setItem('token', token);
@@ -17,4 +26,102 @@ const getToken = () => {
   }
 };
 
-export const signupUser = createAsyncThunk('auth/signupUser');
+//setToken(response.headers.get('Authorization'));
+
+export const signupUser = createAsyncThunk(
+  'auth/signupUser',
+  async (credentials) => await userAPI.signupUser(credentials),
+);
+
+export const loginUser = createAsyncThunk(
+  'auth/loginUser',
+  async (credentials) => await userAPI.loginUser(credentials),
+);
+
+export const checkAuth = createAsyncThunk(
+  'auth/checkAuth',
+  async (token) => await userAPI.checkAuth(getToken()),
+);
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    // might do token stuff here? no!
+    // all my damn reducers are async! deal with it scrubs
+  },
+  extraReducers: {
+    // signup
+    [signupUser.pending]: (state, action) => {
+      state.signup = {
+        ...state.signup,
+        status: 'loading',
+      };
+    },
+    [signupUser.fulfilled]: (state, action) => {
+      state.signup = {
+        ...state.signup,
+        status: 'finished',
+      };
+    },
+    [signupUser.rejected]: (state, action) => {
+      state.signup = {
+        ...state.signup,
+        status: 'failed',
+      };
+    },
+
+    // login
+    [loginUser.pending]: (state, action) => {
+      state.authentication = {
+        ...state.authentication,
+        status: 'loading',
+      };
+    },
+    [loginUser.fulfilled]: (state, action) => {
+      setToken(action.payload.headers.get('Authorization'));
+      state.authentication = {
+        ...state.authentication,
+        authChecked: true,
+        loggedInloggedIn: true,
+        currentUser: action.payload,
+        status: 'finished',
+      };
+    },
+    [loginUser.rejected]: (state, action) => {
+      state.authentication = {
+        ...state.authentication,
+        authChecked: true,
+        loggedIn: false,
+        currentUser: {},
+        status: 'failed',
+      };
+    },
+    // checkAuth
+    [checkAuth.pending]: (state, action) => {
+      state.authentication = {
+        ...state.authentication,
+        status: 'loading',
+      };
+    },
+    [checkAuth.fulfilled]: (state, action) => {
+      state.authentication = {
+        ...state.authentication,
+        authChecked: true,
+        loggedInloggedIn: true,
+        currentUser: action.payload,
+        status: 'finished',
+      };
+    },
+    [checkAuth.rejected]: (state, action) => {
+      state.authentication = {
+        ...state.authentication,
+        authChecked: true,
+        loggedIn: false,
+        currentUser: {},
+        status: 'failed',
+      };
+    },
+  },
+});
+
+export default authSlice.reducer;
