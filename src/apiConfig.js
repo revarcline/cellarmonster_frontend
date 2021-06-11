d7a8caba97332ac23d39cda5bf25f691d6a15947
Alex Cline <rev.a.r.cline@gmail.com>
2021-06-11 15:12:09 -0500

    ya yeee

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,5 +1,5 @@
 // set api root here
 //const apiRoot = 'http://localhost:3001';
-const apiRoot = 'https://cellarmonster.buckar.ooo';
+const apiRoot = 'https://cellarmonster.herokuapp.com';
 
 export default apiRoot;
